3b9850f29bd47227c29058c85f0b5bfa644780bd
evandpradana34rpl-bit <evand_pradana_34rpl@student.smktelkom-mlg.sch.id>
2025-08-05 09:01:05 +0700

    Add files via upload
    biodata evand XR6

---
+++ BiodataEvandXR6.java
@@ -0,0 +1,10 @@
+public class BiodataEvandXR6 {
+  public static void main(String[] args) {
+    System.out.println("Nama : Evand Putra Pradana");
+    System.out.println("Asal Sekolah : SMPN 2 Ngantang");
+    System.out.println("Alamat Rumah : Jl. Segara Anak G7A2 Sawojajar 1");
+    System.out.println("Jenis Kelamin : Laki-Laki");
+    System.out.println("Cita-Cita : Wirausahawan Yang Sukses");
+    System.out.println("Hobi : Memancing, Mendaki Gunung");
+  }
+}
